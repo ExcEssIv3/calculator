df200cf3b352f036fad31f8a7641b2812c02c2b0
Seth Rowland <sethrowland@tamu.edu>
2022-11-22 23:30:32 -0600

    added gitignore

--- src/routes/category.js
+++ src/routes/category.js
@@ -15,6 +15,10 @@ router.get('/:categoryId/contributor/:contributorId', (req, res) => {
     return res.send(req.context.models.categories[req.params.categoryId].contributors.find(contributor => contributor.id === req.params.contributorId));
 });
 
+router.get('/scope/:scopeId', (req, res) => {
+    return res.send(req.params.scopeId);
+});
+
 router.post('/', (req, res) => {
     const id = uuidv4();
     req.context.models.categories[id] = (req.body.category);
