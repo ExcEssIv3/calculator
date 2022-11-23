3798fcbe90d0789f75dde83092f7a275a0fa4bd3
Seth Rowland <sethrowland@tamu.edu>
2022-11-22 23:16:42 -0600

    setup basic routing, extracted models and routes into separate directories

--- src/index.js
+++ src/index.js
@@ -1,74 +1,32 @@
-require('dotenv/config');
-const express = require('express');
-const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-let users = [
-    {
-        id: '1',
-        username: 'TestUser'
-    }
-]
-let categories = [
-    {
-        id: '1',
-        name: 'transportation',
-        contributors: [
-            {
-                id: '1',
-                name: 'truck',
-                carbonOutput: 8
-            }
-        ]
-    },
-    {
-        id: '2',
-        name: 'utilities',
-        contributors: [
-            {
-                id: '1',
-                name: 'electricity',
-                carbonOutput: 50
-            },
-            {
-                id: '2',
-                name: 'water',
-                carbonOutput: 20
-            }
-        ]
-    }
-];
-
+import models from './models/index.js';
+import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => { // pseudo authentication middleware, would be cool to not be pseudo eventually
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
 
 app.get('/', (req, res) => {
     return res.send('Hello world!');
 });
 
-
-
-app.get('/category', (req, res) => {
-    return res.send(JSON.stringify(categories));
-});
-
-app.get('/category/:categoryId', (req, res) => {
-    return res.send(JSON.stringify(categories.find(category => category.id === req.params.categoryId)));
-});
-
-app.get('/category/:categoryId/contributor/:contributorId', (req, res) => {
-    return res.send(JSON.stringify(categories.find(category => category.id === req.params.categoryId).contributors.find(contributor => contributor.id === req.params.contributorId)));
-});
-
-app.post('/category', (req, res) => {
-    categories.push(req.body.category);
-
-    return res.send(category);
-})
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/category', routes.category);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}!`);
---
+++ src/routes/category.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    return res.send(Object.values(req.context.models.categories));
+});
+
+router.get('/:categoryId', (req, res) => {
+    return res.send(req.context.models.categories[req.params.categoryId]);
+});
+
+router.get('/:categoryId/contributor/:contributorId', (req, res) => {
+    return res.send(req.context.models.categories[req.params.categoryId].contributors.find(contributor => contributor.id === req.params.contributorId));
+});
+
+router.post('/', (req, res) => {
+    const id = uuidv4();
+    req.context.models.categories[id] = (req.body.category);
+
+    return res.send(category);
+});
+
+export default router;
